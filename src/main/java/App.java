4c57c42acbb49628ff08e94ade21b3c904f980e8
gleb128 <glebtide@gmail.com>
2024-09-13 00:36:30 +0300

    done x2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat bean2 = (Cat) applicationContext.getBean("Cat");
+        Cat bean3 = (Cat) applicationContext.getBean("Cat");
+        boolean hello = bean.equals(bean1);
+        System.out.println(hello);
+        boolean cat = bean2.equals(bean3);
+        System.out.println(cat);
+
+
+
     }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Cat {
+    private String name;
+    public Cat() {}
+    public Cat(String name, String breed, int age) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
